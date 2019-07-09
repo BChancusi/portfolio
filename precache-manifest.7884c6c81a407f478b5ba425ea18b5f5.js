a024cfa8c7a3e24eb25db7ca46ddfeb6c51624fc
BChancusi <brandonchancusi@gmail.com>
2019-07-09 02:22:37 +0100

    Updates

---
+++ precache-manifest.7884c6c81a407f478b5ba425ea18b5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42d242a2431e92852ebaab108ba0bf4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "db7779d3d527fc12f4ef",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "db7779d3d527fc12f4ef",
+    "url": "/portfolio/static/js/main.627300f2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
