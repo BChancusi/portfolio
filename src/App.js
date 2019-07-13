281d94f4f72808db193072145c2d44d2846873d3
BChancusi <brandonchancusi@gmail.com>
2019-07-13 23:31:55 +0100

    CSS changes
    Improve text sizes based on screen

--- src/App.js
+++ src/App.js
@@ -7,20 +7,16 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <header>
-                    <h1>Brandon Chancusi</h1>
-                    <h2>brandonchancusi@gmail.com</h2>
-                    <div className={"App-links"}>
-                        <a href="https://github.com/BChancusi">
-                            <img src={"GitHub.png"} alt="GitHub"/>
-                        </a>
-                        <a href="https://www.linkedin.com/in/brandon-chancusi-217559172">
-                            <img src={"LinkedIn.png"} alt="LinkedIn" height={"32px"} width={"32px"}/>
-                        </a>
-                    </div>
-
-                </header>
-
+                <h1>Brandon Chancusi</h1>
+                <h2>brandonchancusi@gmail.com</h2>
+                <div className={"App-links"}>
+                    <a href="https://github.com/BChancusi">
+                        <img src={"GitHub.png"} alt="GitHub"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/brandon-chancusi-217559172">
+                        <img src={"LinkedIn.png"} alt="LinkedIn"/>
+                    </a>
+                </div>
             </header>
             <Skills/>
             <hr/>
@@ -30,3 +26,5 @@ function App() {
 }
 
 export default App;
+
+//TODO make header stick
--- src/Project.js
+++ src/Project.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 
 function Projects() {
     return (
@@ -7,40 +6,40 @@ function Projects() {
             <h3>Projects</h3>
             <div className="Projects">
                 <article>
+
                     <h4>Distributed Budgeting System</h4>
                     <a href="https://github.com/BChancusi/distributed">Code</a>
                     <a href="https://distributed-budgeting.herokuapp.com/">Project</a>
 
-                    <p>Web application designed to allow multiple users to input financial data
+                    <p>
+                        Web application designed to allow multiple users to input financial data
                         concurrently leveraging branches and committing.
                     </p>
+                    <p>
+                        Developed using React.js, Express.js and MySQL.
+                    </p>
 
-                    <p>Developed using React.js, Express.js and MySQL.</p>
+                    <img src="https://i.imgur.com/8qfyiT0.png" alt="DBS Demo"/>
 
-                    <img src="https://i.imgur.com/8qfyiT0.png" alt="DBS Demo"
-                         style={{border: "1px solid black"}}/>
                 </article>
                 <article>
                     <h4>Social application</h4>
 
                     <p>Social web appliction allowing users to create posts/comment and create a paid subscription. </p>
-
                     <p>Developed using Vue.js, Firebase, HTML and CSS.</p>
                     <p>Project managed on GitHub with contributions from group members.</p>
                 </article>
                 <article>
                     <h4>SpotFX</h4>
-                    <ul>
-                        <li>
-                            Hackathon created project with six contributors designed to visualise Spotify tracks.
-                        </li>
-                        <li>
-                            Developed using HTML, JQuery and three.js.
-                        </li>
-                        <li>
-                            In charge of interacting with the Spotify API and managing requests.
-                        </li>
-                    </ul>
+                    <p>
+                        Hackathon created project with six contributors designed to visualise Spotify tracks.
+                    </p>
+                    <p>
+                        Developed using HTML, JQuery and three.js.
+                    </p>
+                    <p>
+                        In charge of interacting with the Spotify API and managing requests.
+                    </p>
                 </article>
             </div>
         </section>
