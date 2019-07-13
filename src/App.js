f104de7a44c756194357e3ffece88c93c81bee54
BChancusi <brandonchancusi@gmail.com>
2019-07-13 16:07:23 +0100

    Spaced links and added Skills file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Projects from './Project';
+
 function App() {
     return (
         <div className="App">
@@ -8,12 +9,14 @@ function App() {
                 <header>
                     <h1>Brandon Chancusi</h1>
                     <h2>brandonchancusi@gmail.com</h2>
-                    <a href="https://github.com/BChancusi">
-                        <img src={"GitHub.png"} alt="GitHub"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/brandon-chancusi-217559172">
-                        <img src={"LinkedIn.png"} alt="LinkedIn" height={"32px"} width={"32px"}/>
-                    </a>
+                    <div className={"App-links"}>
+                        <a href="https://github.com/BChancusi">
+                            <img src={"GitHub.png"} alt="GitHub"/>
+                        </a>
+                        <a href="https://www.linkedin.com/in/brandon-chancusi-217559172">
+                            <img src={"LinkedIn.png"} alt="LinkedIn" height={"32px"} width={"32px"}/>
+                        </a>
+                    </div>
 
                 </header>
 
