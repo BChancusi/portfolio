7170d7e4f39beadf2d8e7045b335d1ece3d7c654
BChancusi <brandonchancusi@gmail.com>
2019-07-22 23:53:41 +0100

    Add more space between logos

--- src/Skills.js
+++ src/Skills.js
@@ -6,18 +6,15 @@ function Skills() {
             <h3>Skills</h3>
             <article>
                 <h4>Software development</h4>
-
                 <p>JavaScript, Java, Node.js, React.js, Express.js, Jest, MySQL, HTML, CSS, Git, GitHub,
                     REST, MVC , OOP, TDD, JUnit, Agile, Concurrency.
                 </p>
             </article>
             <article>
                 <h4>Languages</h4>
-
                 <p>English (native), Spanish (fluent). </p>
             </article>
         </section>
-
     );
 }
 
