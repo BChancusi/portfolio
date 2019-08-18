b008a6a485d24cfff3cdb0990f6817b721a609e3
BChancusi <brandonchancusi@gmail.com>
2019-08-18 17:45:12 +0100

    Change project headers font size CSS h3

--- src/Project.js
+++ src/Project.js
@@ -3,10 +3,11 @@ import React from 'react';
 function Projects() {
     return (
         <section>
-            <h3>Projects</h3>
+            <h2>Projects</h2>
             <div className="Projects">
+
                 <article className={"Grey"}>
-                    <h4>Book application</h4>
+                    <h3>Book application</h3>
                     <p>
                         Book application designed to allow users to view, comment, rate and favourite listed books.
                     </p>
@@ -26,34 +27,32 @@ function Projects() {
                         <a className={"Btn"} href="https://github.com/BChancusi/book-application">Code</a>
                     </>
                 </article>
+
                 <article>
-                    <h4>SocialFans</h4>
+                    <h3>SocialFans</h3>
 
                     <p>Social web application allowing users to create posts, comment and create a paid
                         subscription.</p>
                     <p>Developed using Vue.js, Firebase, HTML and CSS.</p>
                     <p>Project managed on GitHub with contributions from group members. </p>
                 </article>
+
                 <article className={"Grey"}>
-                    <h4>Distributed Budgeting System</h4>
-                    <img className={"Project-image"} src="https://i.imgur.com/8qfyiT0.png" alt="DBS Demo"/>
-                    <p>
-                        Single page web app allowing users to manage finances using a
-                        version control system. Created for a client to resolve the issue of multiple users making
-                        changes by providing a consolidated platform.
-                    </p>
-                    <p>
-                        Created login page and user accounts enabling the assignment of user permissions.
-                    </p>
-                    <p>
-                        Tested components using Jest, ensuring test coverage on critical files.
-                    </p>
-                    <p>
-                        This project is built using React.js, with a Express.js backend for handling API requests,
-                        deployed onto Heroku. MySQL provides the database solution with HTML5 and CSS used for elements
-                        and design. Critical components are tested using Jest
-                        ensuring components function as desired.
-                    </p>
+                    <h3>Distributed Budgeting System</h3>
+                            <p>
+                                Single page web application allowing users to manage finances using a
+                                version control system. Created for a client to resolve the issue of multiple users
+                                making changes by providing a consolidated platform.
+                            </p>
+                            <p>
+                                This project is built using React.js, with a Express.js backend for handling API
+                                requests, deployed onto Heroku. MySQL provides the database solution with HTML5 and CSS
+                                used for elements and design. Critical components are tested using Jest
+                                ensuring components function as desired.
+                            </p>
+
+                            <img className={"Project-image"} src="https://i.imgur.com/8qfyiT0.png" alt="DBS Demo"/>
+
                     <div className={"Logos"}>
                         <img className={"Project-logo"} src={"React.png"} alt="DBS Demo"/>
                         <img className={"Project-logo"} src={"Node.png"} alt="DBS Demo"/>
@@ -67,8 +66,9 @@ function Projects() {
                         <a className={"Btn"} href="https://distributed-budgeting.herokuapp.com/">Project</a>
                     </div>
                 </article>
+
                 <article>
-                    <h4>SpotFX</h4>
+                    <h3>SpotFX</h3>
                     <p>
                         Hackathon created project with six contributors designed to visualise Spotify tracks.
                     </p>
--- src/Skills.js
+++ src/Skills.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Skills() {
     return (
         <section>
-            <h3>Skills</h3>
+            <h2>Skills</h2>
             <article>
                 <p>Java – JavaScript – Node.js – React.js – Express.js – Jest – JUnit – MySQL – HTML – CSS
                     – Amazon Web Services (AWS) - Test-driven development (TDD) – Agile software development
